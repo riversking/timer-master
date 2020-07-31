a174f02f93d1c184c6f913b1c35979b2971675bd
riversking <1325010917@qq.com>
2020-07-31 19:22:41 +0800

    dept

--- src/components/dept/DeptTree.vue
+++ src/components/dept/DeptTree.vue
@@ -2,23 +2,33 @@
   <div>
     <el-card class="box-card" style="margin-top: 10px">
       <el-row>
-        <el-col :span="8" style="text-align: right">
-          <el-tree :data="deptTree" default-expand-all  @node-click="handleNodeClick"></el-tree>
+        <el-col :span="24" style="text-align: left">
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-plus" size="small" @click="handleAdd">新增</el-button>
+            <el-button type="primary" icon="el-icon-edit" size="small">编辑</el-button>
+            <el-button type="primary" icon="el-icon-delete" size="small" @click="handleDelete">删除</el-button>
+          </el-button-group>
+        </el-col>
+        <el-col :span="8" style="text-align: right;margin-top: 10px">
+          <el-tree :data="deptTree" default-expand-all @node-click="handleNodeClick"></el-tree>
         </el-col>
         <el-col :span="16" style="text-align: left">
-          <el-form ref="deptForm" :model="deptForm" prop="deptForm" label-width="80px">
-            <el-form-item label="部门名称:" prop="name">
-              <el-input v-model="deptForm.name" style="width: 58%"></el-input>
-            </el-form-item>
+          <el-form ref="deptForm" :model="deptForm" prop="deptForm" label-width="80px" :disabled="disable">
             <el-form-item label="上级部门:" prop="parentId">
               <el-select v-model="deptForm.parentId" placeholder="请选择上级部门">
                 <el-option v-for="item in deptList" :label="item.name" :value="item.id"></el-option>
               </el-select>
             </el-form-item>
+            <el-form-item label="部门名称:" prop="name">
+              <el-input v-model="deptForm.name" style="width: 58%"></el-input>
+            </el-form-item>
             <el-form-item label="排序:" prop="orderNum">
               <el-input-number v-model="deptForm.orderNum" controls-position="right" :min="1"
                                :max="100"></el-input-number>
             </el-form-item>
+            <el-button @click="cancel">取 消</el-button>
+            <el-button type="primary" v-if="edit" @click="addDept">确 定</el-button>
+            <el-button type="primary" v-if="!edit" @click="editDept">编 辑</el-button>
           </el-form>
         </el-col>
       </el-row>
@@ -35,10 +45,13 @@
         deptTree: [],
         deptList: [],
         deptForm: {
+          id: '',
           name: '',
           parentId: '',
           orderNum: ''
-        }
+        },
+        disable: true,
+        edit: true
       };
     },
     mounted() {
@@ -66,8 +79,64 @@
             }
           })
       },
-      handleNodeClick() {
-
+      handleNodeClick(data) {
+        this.deptForm = data
+      },
+      handleAdd() {
+        this.disable = false;
+        this.deptForm.parentId = this.deptForm.id
+        this.deptForm.name = ''
+        this.deptForm.orderNum = ''
+      },
+      addDept() {
+        let obj = {
+          name: this.deptForm.name,
+          parentId: this.deptForm.parentId,
+          orderNum: this.deptForm.orderNum
+        }
+        this.$store.dispatch('addDept', obj)
+          .then(res => {
+            if (res.code === '0') {
+              this.initDept();
+              this.getDeptList();
+            } else {
+              this.$message.warning(res.message);
+            }
+          })
+      },
+      editDept() {
+        let obj = {
+          id: this.deptForm.id,
+          name: this.deptForm.name,
+          parentId: this.deptForm.parentId,
+          orderNum: this.deptForm.orderNum
+        }
+        this.$store.dispatch('editDept', obj)
+          .then(res => {
+            if (res.code === '0') {
+              this.initDept();
+              this.getDeptList();
+            } else {
+              this.$message.warning(res.message);
+            }
+          })
+      },
+      handleDelete() {
+        let obj = {
+          id: this.deptForm.id
+        }
+        this.$store.dispatch('deleteDept', obj)
+          .then(res => {
+            if (res.retCode === 0) {
+              this.initDept();
+              this.getDeptList();
+            } else {
+              this.$message.warning(res.retMsg);
+            }
+          })
+      },
+      cancel() {
+        this.disable = true;
       }
     }
   };
--- src/store/modules/dept.js
+++ src/store/modules/dept.js
@@ -29,6 +29,36 @@ export default {
       } catch (error) {
         console.log('error: ', error)
       }
+    },
+    async addDept({commit}, obj) {
+      try {
+        let res = await postData(`${namespace}/addDept`, obj).catch(err => {
+          commit('GLOBAL_ERR', err, {root: true})
+        });
+        return res.data
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    },
+    async editDept({commit}, obj) {
+      try {
+        let res = await postData(`${namespace}/updateDept`, obj).catch(err => {
+          commit('GLOBAL_ERR', err, {root: true})
+        });
+        return res.data
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    },
+    async deleteDept({commit}, obj) {
+      try {
+        let res = await postData(`${namespace}/deleteDept`, obj).catch(err => {
+          commit('GLOBAL_ERR', err, {root: true})
+        });
+        return res.data
+      } catch (error) {
+        console.log('error: ', error)
+      }
     }
   },
   mutations: {}
