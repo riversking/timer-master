d074dad78d1545ad88c8da6538e10422140f076e
riversking <1325010917@qq.com>
2020-09-11 15:43:13 +0800

    player

---
+++ src/components/player/PlayerBasic.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-card class="box-card" style="margin-top: 10px">
+      <el-col :span="24" style="text-align: right">
+        <el-input type="primary" icon="el-icon-plus" size="small"></el-input>
+      </el-col>
+    </el-card>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'player'
+  }
+</script>
